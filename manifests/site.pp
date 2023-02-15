5d017989e1d5f929afaa0b01e2e510a205661b9e
pholaris <62307601+pholaris@users.noreply.github.com>
2023-02-15 14:03:07 -0600

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,10 +1,2 @@
 node default {
-  file {'/root/README':
-    ensure  => file,
-    content => 'This is my pinche README',
-    owner   => 'root',
-  }
-   file {'/root/README':
-   owner   => 'root',
-  }
 }
